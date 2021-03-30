8dc5e989fae2e3af94ea5dfb97855afe87f5a1c2
HymenopusC <34107739+HymenopusC@users.noreply.github.com>
2021-03-30 14:47:09 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-function calculateTemps(station, date) {
+function calculateTemps(station) {
     let maxTotal = 0;
     let minTotal = 0; 
     for (var i = 0; i < station.length; i++) {
@@ -7,7 +7,7 @@ function calculateTemps(station, date) {
     }
     let maxSlope = maxTotal/station.length;
     let minSlope = minTotal/station.length;
-    let daysSinceLastData = daysSince(date);
+    let daysSinceLastData = daysSince(station[station.length - 1]["DATE"]);
     return [(station[station.length - 1]["TMIN"] + (minSlope * daysSinceLastData)), (station[station.length - 1]["TMAX"] + (maxSlope * daysSinceLastData))];
 }
 
